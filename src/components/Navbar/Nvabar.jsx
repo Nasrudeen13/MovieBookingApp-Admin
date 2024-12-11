6ea73ebc22ad2e48639d148ba1bb26178a148335
Nasrudeen <jnasrudeen13@gmail.com>
2024-12-11 19:39:52 +0530

    first commit

--- src/components/Navbar/Nvabar.jsx
+++ src/components/Navbar/Nvabar.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
-import logo from '/Users/nasrudeen/Desktop/projects/MOVIE BOOKING APP/Images/logo.png'
+import logo from '../Navbar/logo.png'
 import { useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
